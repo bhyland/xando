620355406445901355d66fcceb6b69509acaf7fd
Brendan Hyland <brendan@haiqu.ca>
2020-12-13 07:24:55 -0700

    Preparing for v5

---
+++ src/Evergreen/Migrate/V5.elm
@@ -0,0 +1,36 @@
+module Evergreen.Migrate.V5 exposing (..)
+
+import Evergreen.V1.Types as Old
+import Types as New
+import Lamdera.Migrations exposing (..)
+import Dict
+
+frontendModel : Old.FrontendModel -> ModelMigration New.FrontendModel New.FrontendMsg
+frontendModel old =
+    ModelUnchanged
+
+
+backendModel : Old.BackendModel -> ModelMigration New.BackendModel New.BackendMsg
+backendModel old =
+    ModelMigrated ({ clientList = old.clientList
+                   , sessions = Dict.empty }, Cmd.none)
+
+
+frontendMsg : Old.FrontendMsg -> MsgMigration New.FrontendMsg New.FrontendMsg
+frontendMsg old =
+    MsgUnchanged
+
+
+toBackend : Old.ToBackend -> MsgMigration New.ToBackend New.BackendMsg
+toBackend old =
+    MsgUnchanged
+
+
+backendMsg : Old.BackendMsg -> MsgMigration New.BackendMsg New.BackendMsg
+backendMsg old =
+    MsgUnchanged
+
+
+toFrontend : Old.ToFrontend -> MsgMigration New.ToFrontend New.FrontendMsg
+toFrontend old =
+    MsgUnchanged
